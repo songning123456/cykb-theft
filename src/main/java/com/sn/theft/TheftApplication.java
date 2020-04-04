be7741a4fd8d7a688727be2d6b680bc13c708ee6
songning <1457065857@qq.com>
2020-04-04 15:29:48 +0800

    git add

---
+++ src/main/java/com/sn/theft/TheftApplication.java
@@ -0,0 +1,13 @@
+package com.sn.theft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheftApplication.class, args);
+    }
+
+}
