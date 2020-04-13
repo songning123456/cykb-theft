93990a5a4c27e1c2377e2fdb405d226747e0fe9c
songning <1457065857@qq.com>
2020-04-13 16:01:07 +0800

    优化

--- src/main/java/com/sn/theft/repository/NovelsRepository.java
+++ src/main/java/com/sn/theft/repository/NovelsRepository.java
@@ -2,7 +2,6 @@ package com.sn.theft.repository;
 
 import com.sn.theft.entity.Novels;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,9 +10,6 @@ import java.util.List;
  * @date: 2020/3/9 23:01
  */
 public interface NovelsRepository extends JpaRepository<Novels, String> {
-
-    @Query(value = "select * from novels where source_name = ?1 order by create_time desc limit ?2", nativeQuery = true)
-    List<Novels> findFirstClassifyNative(String sourceName, int size);
 
     List<Novels> findBySourceUrl(String sourceUrl);
 }
